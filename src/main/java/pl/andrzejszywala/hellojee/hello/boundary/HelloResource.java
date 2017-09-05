538b7c23d05bf384984c08221e3ce5b7614a1b89
Andrzej <Andrzej@192.168.16.1>
2017-09-05 17:55:15 +0200

    logging server port

--- src/main/java/pl/andrzejszywala/hellojee/hello/boundary/HelloResource.java
+++ src/main/java/pl/andrzejszywala/hellojee/hello/boundary/HelloResource.java
@@ -13,10 +13,13 @@ import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import org.hibernate.engine.spi.SessionImplementor;
@@ -33,14 +36,16 @@ public class HelloResource {
 	EntityManager em;
 
 	@Inject
-	Logger tracer;
-
+	Logger log;
+	
 	@GET
-	public JsonObject hello() throws SQLException, UnknownHostException {
+	public JsonObject hello(@Context HttpServletRequest request) throws SQLException, UnknownHostException {
+		log.info("HELLO JEE");
 		DatabaseMetaData metadata = ((SessionImplementor) em.getDelegate()).connection().getMetaData();
 		return Json.createObjectBuilder()
 				   .add("database", metadata.getDatabaseProductName() + " - " + metadata.getDatabaseProductVersion())
 		           .add("host", InetAddress.getLocalHost().getHostName() + " (" + InetAddress.getLocalHost().getHostAddress() + ")")
+		           .add("port", request.getServerPort())
 		           .add("hello", em.createNamedQuery(Hello.findAll, Hello.class)
 		        		           .getResultList()
 		        		           .stream()
